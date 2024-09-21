7b848249c2b4c6475ace0cc0629b89af24f6fce7
Jrosero21 <Jnrosero@gmail.com>
2024-09-21 16:09:07 -0700

    added create account link to home page ande made layout changes

--- public/home.js
+++ public/home.js
@@ -1,17 +1,23 @@
-function Home(){
+function Home() {
   return (
     <>
-      <h1 className="pageTitle"> Welcome to the bank </h1>
-      <div className="d-flex justify-content-center">
-        <Card
-          txtcolor="black"
-          header="BadBank Landing Module"
-          title="Welcome to the bank"
-          text="Please login or create an account to utilize our services"
-          body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
-          headerClass="bg-white"
-          className="card-border"
-        />
+      <div className="d-flex justify-content-center flex-column align-items-center">
+        {/* Center the title above both the card and the link */}
+        <h1 className="pageTitle"> Welcome to the bank </h1>
+
+        {/* Container for the card and the link */}
+        <div className="d-flex justify-content-between align-items-center" style={{ width: '80%' }}>
+          <Card
+            txtcolor="black"
+            header="BadBank Landing Module"
+            title="Welcome to the bank"
+            text="Please login or create an account to utilize our services"
+            body={(<img src="bank.png" className="img-fluid" alt="Responsive image" />)}
+            headerClass="bg-white"
+            className="card-border"
+          />
+          <Link to="/CreateAccount" className="create-account-link">Create an account today!</Link>
+        </div>
       </div>
     </>
   );
