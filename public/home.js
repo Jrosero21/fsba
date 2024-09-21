0e69cd13aa41887c08958f6499f9689cade363ae
Jrosero21 <Jnrosero@gmail.com>
2024-09-21 14:15:31 -0700

    ui testing

--- public/home.js
+++ public/home.js
@@ -1,13 +1,16 @@
 function Home(){
   return (
-    <div className="d-flex justify-content-center">
-      <Card
-        txtcolor="black"
-        header="BadBank Landing Module"
-        title="Welcome to the bank"
-        text="Please login or create an account to utilize our services"
-        body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
-      />
-    </div>
+    <>
+      <h1 className="pageTitle"> Welcome to the bank </h1>
+      <div className="d-flex justify-content-center">
+        <Card
+          txtcolor="black"
+          header="BadBank Landing Module"
+          title="Welcome to the bank"
+          text="Please login or create an account to utilize our services"
+          body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+        />
+      </div>
+    </>
   );
 }
