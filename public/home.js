9cab884ea6b0368e439d57682d08f34d747ddfb6
Jrosero21 <Jnrosero@gmail.com>
2024-09-21 17:57:42 -0700

    made text changes

--- public/home.js
+++ public/home.js
@@ -1,24 +1,19 @@
 function Home() {
   return (
-    <>
-      <div className="d-flex justify-content-center flex-column align-items-center">
-        {/* Center the title above both the card and the link */}
-        <h1 className="pageTitle"> Welcome to the bank </h1>
-
-        {/* Container for the card and the link */}
-        <div className="d-flex justify-content-between align-items-center" style={{ width: '80%' }}>
+    <div className="home-container">
+      <h1 className="pageTitle">Welcome to the bank</h1>
+      <div className="content">
+        <div className="d-flex justify-content-center">
           <Card
             txtcolor="black"
             header="BadBank Landing Module"
             title="Welcome to the bank"
             text="Please login or create an account to utilize our services"
             body={(<img src="bank.png" className="img-fluid" alt="Responsive image" />)}
-            headerClass="bg-white"
-            className="card-border"
           />
-          <Link to="/CreateAccount" className="create-account-link">Create an account today!</Link>
         </div>
+        <a href="#/CreateAccount/" className="create-account-link">Create an account today!</a>
       </div>
-    </>
+    </div>
   );
 }
