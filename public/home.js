4b94eb69b9f8a7005ba04f39b24e6a674aa466b3
Jrosero21 <jnrosero@gmail.com>
2024-09-22 18:29:49 -0700

    ui changes

--- public/home.js
+++ public/home.js
@@ -1,19 +1,29 @@
 function Home() {
   return (
-    <div className="home-container">
-      <div className="content">
-        <Card
-          txtcolor="black"
-          header="BadBank Credit Union"  // This header will be centered
-          title="Welcome to the bank"
-          text="Please login or create an account to utilize our services"
-          body={(<img src="bank1.png" className="img-fluid" alt="Responsive image" />)}
-          className="card-border"
-        />
-        <a href="#/CreateAccount/" className="create-account-link">
-          Create an account today!
-        </a>
+    <div className="container">
+      <div className="row align-items-start">
+        {/* Left-side image */}
+        <div className="col-md-4 text-center"> {/* Center the image horizontally */}
+          <img src="bb-purp.png" className="img-fluid left-image" alt="BB Purple Logo" />
+        </div>
+        {/* Center Card */}
+        <div className="col-md-8">
+          <div className="content">
+            <Card
+              txtcolor="black"
+              header="BadBank Credit Union"  // This header will be centered
+              title="Welcome to the bank"
+              text="Please login or create an account to utilize our services"
+              body={(<img src="bank1.png" className="img-fluid" alt="Responsive image" />)}
+              className="card-border"
+            />
+            <a href="#/CreateAccount/" className="create-account-link">
+              Create an account today!
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
+
